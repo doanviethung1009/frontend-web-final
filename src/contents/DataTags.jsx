1687b3a81bd08a93b3c16b1eca34019539f10be3
longlh <longlh@runsystem.net>
2024-11-20 16:24:35 +0700

    BLOCK_SEARCH_BAR

---
+++ src/contents/DataTags.jsx
@@ -0,0 +1,172 @@
+const DataTags = [
+  {
+    key: "VIETNAM_TRAVEL",
+    labelKey: "vietnam_travel",
+  },
+  {
+    key: "OVERSEAS_TRAVEL",
+    labelKey: "overseas_travel",
+  },
+  {
+    key: "REGIONAL_TRAVEL",
+    labelKey: "regional_travel",
+  },
+  {
+    key: "ADVENTURE",
+    labelKey: "adventure",
+  },
+  {
+    key: "BOAT_TRIPS",
+    labelKey: "boat_trips",
+  },
+  {
+    key: "CULTURAL_EXPERIENCE",
+    labelKey: "cultural_experience",
+  },
+  {
+    key: "ECO_FRIENDLY",
+    labelKey: "eco_friendly",
+  },
+  {
+    key: "SUSTAINABLE_TOURISM",
+    labelKey: "sustainable_tourism",
+  },
+  {
+    key: "PHOTOGRAPHY_TOURS",
+    labelKey: "photography_tours",
+  },
+  {
+    key: "WILDLIFE_ENCOUNTERS",
+    labelKey: "wildlife_encounters",
+  },
+  {
+    key: "SPIRITUAL_TRAVEL",
+    labelKey: "spiritual_travel",
+  },
+  {
+    key: "EDUCATIONAL_TRAVEL",
+    labelKey: "educational_travel",
+  },
+  {
+    key: "VOLUNTEER_TOURISM",
+    labelKey: "volunteer_tourism",
+  },
+  {
+    key: "BEACH",
+    labelKey: "beach",
+  },
+  {
+    key: "MOUNTAIN",
+    labelKey: "mountain",
+  },
+  {
+    key: "HERITAGE_SITES",
+    labelKey: "heritage_sites",
+  },
+  {
+    key: "HIKING",
+    labelKey: "hiking",
+  },
+  {
+    key: "COUNTRYSIDE_ESCAPES",
+    labelKey: "countryside_escapes",
+  },
+  {
+    key: "HISTORICAL_SITES",
+    labelKey: "historical_sites",
+  },
+  {
+    key: "URBAN_EXPLORATION",
+    labelKey: "urban_exploration",
+  },
+  {
+    key: "DESERT_TOURS",
+    labelKey: "desert_tours",
+  },
+  {
+    key: "CITY_TOURS",
+    labelKey: "city_tours",
+  },
+  {
+    key: "GROUP_TOURS",
+    labelKey: "group_tours",
+  },
+  {
+    key: "CYCLING_TOURS",
+    labelKey: "cycling_tours",
+  },
+  {
+    key: "FAMILY_TRIPS",
+    labelKey: "family_trips",
+  },
+  {
+    key: "COUPLE_RETREATS",
+    labelKey: "couple_retreats",
+  },
+  {
+    key: "PRIVATE_TOURS",
+    labelKey: "private_tours",
+  },
+  {
+    key: "SELF_GUIDED_TOURS",
+    labelKey: "self_guided_tours",
+  },
+  {
+    key: "YOUTH_TRIPS",
+    labelKey: "youth_trips",
+  },
+  {
+    key: "LUXURY_TRAVEL",
+    labelKey: "luxury_travel",
+  },
+  {
+    key: "PREMIUM_TRAVEL",
+    labelKey: "premium_travel",
+  },
+  {
+    key: "ECONOMY_TRAVEL",
+    labelKey: "economy_travel",
+  },
+  {
+    key: "BACKPACKING",
+    labelKey: "backpacking",
+  },
+  {
+    key: "SOLO_TRAVEL",
+    labelKey: "solo_travel",
+  },
+  {
+    key: "LOCAL_CUISINE",
+    labelKey: "local_cuisine",
+  },
+  {
+    key: "WELLNESS_RETREATS",
+    labelKey: "wellness_retreats",
+  },
+  {
+    key: "GASTRONOMY_TOURS",
+    labelKey: "gastronomy_tours",
+  },
+  {
+    key: "SPA_AND_RELAXATION",
+    labelKey: "spa_and_relaxation",
+  },
+  {
+    key: "FESTIVAL_TOURS",
+    labelKey: "festival_tours",
+  },
+  {
+    key: "HOLIDAY",
+    labelKey: "holiday",
+  },
+  {
+    key: "SEASONAL_TOURS",
+    labelKey: "seasonal_tours",
+  },
+  {
+    key: "EVENT_BASED_TOURS",
+    labelKey: "event_based_tours",
+  },
+];
+
+export default DataTags;
